d1f37ff335f5937288fd416713fe8c8261af8b21
NyaNyagulugulu <support@cnmsb.xin>
2026-05-27 00:20:30 +0800

    rm qt-win

--- qt-win/6.10.2/mingw_64/bin/qtenv2.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Setting up environment for Qt usage...
-set PATH=/home/neko/qt-win/6.10.2/mingw_64/bin;%PATH%
-cd /D /home/neko/qt-win/6.10.2/mingw_64
-echo Remember to call vcvarsall.bat to complete environment setup!
